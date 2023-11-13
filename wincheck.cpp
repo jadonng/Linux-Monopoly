47ca74c0f2f94ced91ba9028168ec5899ac1963e
swh2004 <146440509+swh2004@users.noreply.github.com>
2023-11-13 22:41:52 +0800

    Update wincheck.cpp

--- wincheck.cpp
+++ wincheck.cpp
@@ -5,10 +5,10 @@
 #include "wincheck.h"
 using namespace std;
 
-/*A function to check if p1 or p2 wins
-if one of them wins, return true,
-else return false
-*/
+//A function to check if p1 or p2 wins
+//if one of them wins, return true,
+//else return false
+
 bool wincheck(players p1, players p2){
     const int money_to_win=0;
     if(p1.money >= money_to_win || p2.money <= 0){
