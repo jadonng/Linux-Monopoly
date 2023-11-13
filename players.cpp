a353654b983fed2394f994270ba3f495fdbe82a2
jadonng <145362875+jadonng@users.noreply.github.com>
2023-11-13 23:58:13 +0800

    Update players.cpp
    updated casing for convention

--- players.cpp
+++ players.cpp
@@ -3,7 +3,7 @@
 #include "players.h"
 #include "board.h"
 
-struct player {
+struct Player {
   /* Explanation:
      number:    player number, e.g. number = 1 for player 1
      *pos:      position, e.g. pos = &board[10] if the player is at cell 10
@@ -12,7 +12,7 @@ struct player {
                 (e.g., if the player purchased land at cell c3 and c19, then land_list = {3, 19})
   */
 	char number;
-	cell *pos;
+	Cell *pos;
 	int money;
 	std::vector<int> land_list;
 };
