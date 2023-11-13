2c322eff662feb2a2819b0bb00a5204ea557cde8
宋文皓 <baigao@songwenhaodeMacBook-Pro.local>
2023-11-13 22:23:24 +0800

    wincheck

--- wincheck.cpp
+++ wincheck.cpp
@@ -1,1 +1,23 @@
 //wincheck.cpp
+#include <iostream>
+#include <string>
+#include "players.h"
+#include "wincheck.h"
+using namespace std;
+
+/*A function to check if p1 or p2 wins
+if one of them wins, return true,
+else return false
+*/
+bool wincheck(players p1, players p2){
+    const int money_to_win=0;
+    if(p1.money >= money_to_win || p2.money <= 0){
+        cout<<"Player 1 wins!"<<"\n";
+        return 1;
+    }
+    else if(p2.money >= money_to_win || p1.money <= 0){
+        cout<<"Player 2 wins!"<<"\n";
+        return 1;
+    }
+    return 0;
+}
