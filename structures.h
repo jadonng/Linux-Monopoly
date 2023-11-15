7d3050db49528bc8116c9b6bb5bc8ac3538e46ff
swh2004 <146440509+swh2004@users.noreply.github.com>
2023-11-16 06:48:15 +0800

    Update structures.h

--- structures.h
+++ structures.h
@@ -34,11 +34,17 @@ struct Cell{
         switch (type) {
 	        case 0:
             // event to be triggered if player land on land
-		if(player.pos -> owner != "Bank"){
-			cout << "You have arrived " << player.pos -> name << " which is owned by " << player.pos -> owner << " with the rent of " << player.pos -> rent <<endl;
+		//if the land is owned by player
+		if(player.pos -> owner == player.name){
+			cout << "You have arrived " << player.pos -> name << " which is owned by you with the rent of " << player.pos -> rent <<endl;
 		}
-		else{
+		//if the land is owned by bank
+		else if(player.pos -> owner == "bank"){
 			cout << "You have arrived " << player.pos -> name << " which has no owner with the price of " << player.pos -> price << endl; 
+		}
+		//if the land is owned by other player
+		else{
+			cout << "You have arrived " << player.pos -> name << " which is owned by " << player.pos -> owner << " with the rent of " << player.pos -> rent <<endl;
 		}
 		break;
             case 1: 
@@ -96,22 +102,26 @@ struct Cell{
 	            }
             case 4:
 	        // event to be triggered if player land on jail
+		//if the player was sent to jail last time and skipped a rolling dice
 		if(player.in_jail){
-			cout << "Now you are released, you can move next turn!" << endl;//if could use in_jail to skip the actionbeforeroll and call triggerevent
+			cout << "Now you are released, you can move next turn!" << endl;
 			player.in_jail = false;
 		}
+		//move to the jail with rolling dice
 		else{
-			cout << "You arrive on jail. Nothing happens." << endl;//move to the jail with rolling dice
+			cout << "You arrive on jail. Nothing happens." << endl;
 		}
 		break;
             case 5:
 	        // event to be triggered if player land on go to jail
 		cout << "You have encountered legal cases, now it's time to go to the jail." << endl;
+		//if has card could choose to use or not
 		if(player.has_card){
-			cout << "You have got " << player.has_card << " jail card(s)." << endl;//has card can choose to use or not
+			cout << "You have got " << player.has_card << " jail card(s)." << endl;
 		}
+		//if no card, just arrested
 		else{
-			cout << "You are in jail now, rest for one turn." << endl;//no card just arrested
+			cout << "You are in jail now, rest for one turn." << endl;
 			player.pos = &board[20];//send to jail
 			player.in_jail = true;
 		}
