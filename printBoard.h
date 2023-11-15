918b0bfca55d097ae266146f320178621d735a40
JoeCFKwok <146162566+JoeCFKwok@users.noreply.github.com>
2023-11-15 14:37:30 +0800

    Update printBoard.h

--- printBoard.h
+++ printBoard.h
@@ -1,1 +1,22 @@
 //printBoard.h
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Cell {
+    string name;
+    int price;
+};
+
+class Board {
+public:
+    vector<Cell> cells;
+
+    // Constructor
+    Board(vector<Cell> cells) : cells(cells) {}
+
+    // Method to print the board
+    void printBoard();
+};
