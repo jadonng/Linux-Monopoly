ac8292c9d60350a7663fa46b0e77a86b91bf2ed1
swh2004 <146440509+swh2004@users.noreply.github.com>
2023-11-15 18:15:02 +0800

    Update wincheck.cpp

--- wincheck.cpp
+++ wincheck.cpp
@@ -1,7 +1,7 @@
 //wincheck.cpp
 #include <iostream>
 #include <string>
-#include "players.h"
+#include "structures.h"
 #include "wincheck.h"
 using namespace std;
 
@@ -13,11 +13,11 @@ bool wincheck(Player p1, Player p2){
     const int money_to_win=1000;
     if(p1.money >= money_to_win || p2.money <= 0){
         cout<<"Player 1 wins!"<<endl;
-        return 1;
+        return true;
     }
     else if(p2.money >= money_to_win || p1.money <= 0){
         cout<<"Player 2 wins!"<<endl;
-        return 1;
+        return true;
     }
-    return 0;
+    return false;
 }
