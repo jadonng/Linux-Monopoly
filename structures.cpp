be5578dcf803a89d7ee02de3944c221c3c9811e8
swh2004 <146440509+swh2004@users.noreply.github.com>
2023-11-19 21:16:26 +0800

    add break and modify structure

--- structures.cpp
+++ structures.cpp
@@ -36,6 +36,7 @@ void TriggerEvent(Player &player, Cell board[], Player player_array[]) {
         case 1: 
 	        // event to be triggered if player land on starting
 		cout << "You have arrived the starting point; welcome back!" << endl;
+		break;
         case 2: 
 	        // event to be triggered if player land on punishment
 		int tax;
@@ -51,6 +52,7 @@ void TriggerEvent(Player &player, Cell board[], Player player_array[]) {
 		}	
 		cout << "You need to pay " << tax << " for the " << player.pos -> name << endl;
 		player.money -= tax;
+		break;
 	case 3:
 	        // event to be triggered if player land on chance
 	        srand(time(NULL));
@@ -99,30 +101,30 @@ void TriggerEvent(Player &player, Cell board[], Player player_array[]) {
 		    break;
 
         case 4:
-          	        // event to be triggered if player land on jail
-          		//if the player was sent to jail last time and skipped a rolling dice
-          		if(player.in_jail){
-          			cout << "Now you are released, you can move next turn!" << endl;
-          			player.in_jail = false;
-          		}
-          		//move to the jail with rolling dice
-          		else{
-          			cout << "You arrive on jail. Nothing happens." << endl;
-          		}
-          		break;
+		// event to be triggered if player land on jail
+		//if the player was sent to jail last time and skipped a rolling dice
+		if(player.in_jail){
+			cout << "Now you are released, you can move next turn!" << endl;
+			player.in_jail = false;
+		}
+		//move to the jail with rolling dice
+		else{
+			cout << "You arrive on jail. Nothing happens." << endl;
+		}
+		break;
         case 5:
-          	        // event to be triggered if player land on go to jail
-          		cout << "You have encountered legal cases, now it's time to go to the jail." << endl;
-          		//if has card could choose to use or not
-          		if(player.num_card){
-          			cout << "You have got " << player.num_card << " jail card(s)." << endl;
-          		}
-          		//if no card, just arrested
-          		else{
-          			cout << "You are in jail now, rest for one turn." << endl;
-          			player.pos = &board[20];//send to jail
-          			player.in_jail = true;
-          		}
-          		break;
-                  }
-              }
+		// event to be triggered if player land on go to jail
+		cout << "You have encountered legal cases, now it's time to go to the jail." << endl;
+		//if has card could choose to use or not
+		if(player.num_card){
+			cout << "You have got " << player.num_card << " jail card(s)." << endl;
+		}
+		//if no card, just arrested
+		else{
+			cout << "You are in jail now, rest for one turn." << endl;
+			player.pos = &board[20];//send to jail
+			player.in_jail = true;
+		}
+		break;
+        }
+}
