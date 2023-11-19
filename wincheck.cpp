5b3e89ab87e01555b9d6f2a9572ed47e68c7f40a
swh2004 <146440509+swh2004@users.noreply.github.com>
2023-11-19 12:06:22 +0800

    Update wincheck.cpp

--- wincheck.cpp
+++ wincheck.cpp
@@ -5,7 +5,7 @@
 #include "wincheck.h"
 using namespace std;
 
-//A function to check if p1 or p2 wins
+//A function to check if player p in arr is win.
 //if one of them wins, return true,
 //else return false
 
@@ -18,8 +18,9 @@ bool wincheck(Player arr[]){
         cout<<"Player " << p.name << " wins!"<<endl;
         return true;
     }
-    }
-    if(p1.money >= money_to_win || p2.money <= 0){
+    return false;
+}
+/*if(p1.money >= money_to_win || p2.money <= 0){
         cout<<"Player 1 wins!"<<endl;
         return true;
     }
@@ -28,4 +29,17 @@ bool wincheck(Player arr[]){
         return true;
     }
     return false;
+}*/
+
+//A function to check if player p is broke
+//if so return true to eliminate player p
+bool brokecheck(Player p){
+    if(p.money < 0){
+        cout << "Player " << p.name << " is broke." << endl;
+        return true;
+    }
+return false;
 }
+
+
+
