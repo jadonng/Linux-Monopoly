d5af306baa0df548a55ebbfe748113095d9937ab
swh2004 <146440509+swh2004@users.noreply.github.com>
2023-11-19 22:17:46 +0800

    add >>

--- checkstatus.cpp
+++ checkstatus.cpp
@@ -9,10 +9,10 @@ using namespace std;
 // A function to check the player's status,
 // print out player's name, position, money, and lands
 void checkstatus(Player p, Cell board[]){
-    cout<<"Player: "<<p.name<<"\n"<<"Your Position: "<<p.pos -> ID<<"\n"<<"Your Money: "<<p.money<<"\n";
-    cout<<"Your Land: ";
+    cout<<">> Player: "<<p.name<<"\n"<<"Your Position: "<<p.pos -> ID<<"\n"<<"Your Money: "<<p.money<<"\n";
+    cout<<">> Your Land: ";
     for(int i : p.land_list) {
-        cout<<board[i].name;
+        cout<<">> " <<board[i].name;
         cout<<" ";
     }
     cout<<"\n";
