42b493bf35b459ba91cbdef4dbfd3c4f51be8d2f
swh2004 <146440509+swh2004@users.noreply.github.com>
2023-11-19 21:41:23 +0800

    Update wincheck.h

--- wincheck.h
+++ wincheck.h
@@ -4,7 +4,7 @@
 #include "structures.h"
 using namespace std;
 
-bool wincheck(Player *arr, int num_of_player);
+void outloop_wincheck(Player *arr, int num_of_player);
 bool brokecheck(Player p);
 
 #endif
