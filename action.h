e551fd4fd7988542bace54c071f87f1249507b69
TszChungPOON <u3607777@connect.hku.hk>
2023-11-19 21:03:54 +0800

    Update action.h

--- action.h
+++ action.h
@@ -7,4 +7,5 @@
 #define ACTION_H
 void buy(Cell Board[40], Player player);
 void payrent(Cell Board[40], Player playerstep, Player playerown);
+void buildproperty(Cell Board[40], Player playerstep);
 #endif
