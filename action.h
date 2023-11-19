f878079d4c746b771876f970799bd993d1dab1ca
jadonng <145362875+jadonng@users.noreply.github.com>
2023-11-20 00:15:21 +0800

    Update action.h

--- action.h
+++ action.h
@@ -5,7 +5,7 @@
 
 #ifndef ACTION_H
 #define ACTION_H
-void buy(Cell Board[40], Player player);
-void payrent(Cell Board[40], Player playerstep, Player playerown);
-void buildproperty(Cell Board[40], Player playerstep);
+void buy(Cell Board[40], Player &player);
+void payrent(Cell Board[40], Player &playerstep, Player &playerown);
+void buildproperty(Cell Board[40], Player &playerstep);
 #endif
