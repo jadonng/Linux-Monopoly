63eaf14c11209075769e77ea81e91516c41232f7
swh2004 <146440509+swh2004@users.noreply.github.com>
2023-11-19 22:24:52 +0800

    Update checkstatus.cpp

--- checkstatus.cpp
+++ checkstatus.cpp
@@ -10,9 +10,9 @@ using namespace std;
 // print out player's name, position, money, and lands
 void checkstatus(Player p, Cell board[]){
     cout<<">> Player: "<<p.name<<"\n"<<"Your Position: "<<p.pos -> ID<<"\n"<<"Your Money: "<<p.money<<"\n";
-    cout<<">> Your Land: ";
+    cout<<">> Your Land(properties): ";
     for(int i : p.land_list) {
-        cout<<">> " <<board[i].name;
+        cout<<">> " <<board[i].name<< "("<<board[i].property<<")";
         cout<<" ";
     }
     cout<<"\n";
