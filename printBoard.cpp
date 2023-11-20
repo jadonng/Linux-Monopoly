3e92d1ff6734477e25378c73bbb45cedad523965
JoeCFKwok <146162566+JoeCFKwok@users.noreply.github.com>
2023-11-20 20:55:33 +0800

    Update printBoard.cpp

--- printBoard.cpp
+++ printBoard.cpp
@@ -21,7 +21,7 @@ void loadBoard(Cell Board[BOARD_SIZE]) {
         Board[i].ID = i;
         Board[i].name = cellNames[i];  // Set the name from the cellNames array
     }
-    
+}
 void printBoard(Cell Board[BOARD_SIZE]) {
     int side = BOARD_SIZE / 4;
 
