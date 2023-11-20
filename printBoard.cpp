865f24a6d92b21c03808b4993e5228cd13b3f14e
JoeCFKwok <146162566+JoeCFKwok@users.noreply.github.com>
2023-11-20 21:43:37 +0800

    Update printBoard.cpp

--- printBoard.cpp
+++ printBoard.cpp
@@ -7,6 +7,23 @@
 const int BOARD_SIZE = 40;
 
 void printBoard(Cell Board[]) {
+    srand(time(0));  // Initialize random seed
+
+    // Array of cell names
+    std::string cellNames[BOARD_SIZE] = {
+        "Go", "Graduate_House", "Chance", "KK_Leung", "Income_Tax", "MTR_Exit_A1", "Meng_Wah_Complex", "Chance", "Hawking_Wong_Building", "Run_Run_Shaw_Building",
+        "Jail", "Main_Library", "Swire_Canteen", "Knowles_Building", "Library_Extension", "MTR_Exit_A2", "Chong_Yuet_Ming", "Chance", "T.T.Tsui_Building", "Kadoorie_Building",
+        "Jail", "Main_Building", "Chance", "Composite_Building", "Run_Run_Shaw_Tower", "MTR_EXIT_B", "The_Jockey_Club_Tower", "Cheng_Yu_Tung_Building", "SU_Canteen", "Eilot Hall",
+        "Go_To_Jail", "Libaray_Extension", "May_Hall", "Chance", "Central_Podium_Levels", "MTR_Exit_C", "Chance", "Med_Campus", "Luxury_Tax", "Dental_Campus"
+    };
+
+    // Load the board
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        Board[i].ID = i;
+        Board[i].name = cellNames[i];  // Set the name from the cellNames array
+
+    }
+    // Print the board
     int side = BOARD_SIZE / 4;
 
     // Print top row
