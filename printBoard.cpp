7310aa7617b8ee6f56f47bcb3c3c150ba7e7fefb
JoeCFKwok <146162566+JoeCFKwok@users.noreply.github.com>
2023-11-21 20:46:26 +0800

    Update printBoard.cpp

--- printBoard.cpp
+++ printBoard.cpp
@@ -1,34 +1,19 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>  // Include this library
 #include "printBoard.h"
 #include "structures.h"
 #include "board.h"
 
 const int BOARD_SIZE = 40;
+const int CELL_WIDTH = 20;  // Define a constant for cell width
 
 void printBoard(Cell Board[]) {
-    srand(time(0));  // Initialize random seed
-
-    // Array of cell names
-    std::string cellNames[BOARD_SIZE] = {
-        "Go", "Graduate_House", "Chance", "KK_Leung", "Income_Tax", "MTR_Exit_A1", "Meng_Wah_Complex", "Chance", "Hawking_Wong_Building", "Run_Run_Shaw_Building",
-        "Jail", "Main_Library", "Swire_Canteen", "Knowles_Building", "Library_Extension", "MTR_Exit_A2", "Chong_Yuet_Ming", "Chance", "T.T.Tsui_Building", "Kadoorie_Building",
-        "Jail", "Main_Building", "Chance", "Composite_Building", "Run_Run_Shaw_Tower", "MTR_EXIT_B", "The_Jockey_Club_Tower", "Cheng_Yu_Tung_Building", "SU_Canteen", "Eilot Hall",
-        "Go_To_Jail", "Libaray_Extension", "May_Hall", "Chance", "Central_Podium_Levels", "MTR_Exit_C", "Chance", "Med_Campus", "Luxury_Tax", "Dental_Campus"
-    };
-
-    // Load the board
-    for (int i = 0; i < BOARD_SIZE; i++) {
-        Board[i].ID = i;
-        Board[i].name = cellNames[i];  // Set the name from the cellNames array
-
-    }
-    // Print the board
     int side = BOARD_SIZE / 4;
 
     // Print top row
-    for (int i = 0; i < side; i++) {
-        std::cout << "| " << Board[i].name;
+    for (int i = 0; i <= side; i++) {  // Change here
+        std::cout << "| " << std::setw(CELL_WIDTH) << std::left << Board[i].name;
         if (Board[i].type == 0) {  // If the cell is a Land
             std::cout << " ($" << Board[i].price << ")";
         }
@@ -36,15 +21,15 @@ void printBoard(Cell Board[]) {
     std::cout << "|" << std::endl;
 
     // Print cells from the left and right columns
-    for (int i = 1; i < side - 1; i++) {
-        std::cout << "| " << Board[BOARD_SIZE - i].name;
-        if (Board[BOARD_SIZE - i].type == 0) {  // If the cell is a Land
-            std::cout << " ($" << Board[BOARD_SIZE - i].price << ")";
+    for (int i = 1; i < side; i++) {  // No change here
+        std::cout << "| " << std::setw(CELL_WIDTH) << std::left << Board[3*side - i].name;
+        if (Board[3*side - i].type == 0) {  // If the cell is a Land
+            std::cout << " ($" << Board[3*side - i].price << ")";
         }
         for (int j = 0; j < side - 2; j++) {
             std::cout << "|     ";
         }
-        std::cout << "| " << Board[side + i].name;
+        std::cout << "| " << std::setw(CELL_WIDTH) << std::left << Board[side + i].name;
         if (Board[side + i].type == 0) {  // If the cell is a Land
             std::cout << " ($" << Board[side + i].price << ")";
         }
@@ -52,8 +37,8 @@ void printBoard(Cell Board[]) {
     }
 
     // Print bottom row
-    for (int i = 0; i < side; i++) {
-        std::cout << "| " << Board[2*side + i].name;
+    for (int i = 0; i <= side; i++) {  // Change here
+        std::cout << "| " << std::setw(CELL_WIDTH) << std::left << Board[2*side + i].name;
         if (Board[2*side + i].type == 0) {  // If the cell is a Land
             std::cout << " ($" << Board[2*side + i].price << ")";
         }
