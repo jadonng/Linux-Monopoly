cb19ed62baf665abb044d0182d5988953c0bb15d
swh2004 <146440509+swh2004@users.noreply.github.com>
2023-11-23 02:10:31 +0800

    Update wincheck.cpp

--- wincheck.cpp
+++ wincheck.cpp
@@ -16,15 +16,12 @@ void outloop_wincheck(Player *arr, int num_of_player, Cell board[]){
     vector<string> winner;
     
     for(int i = 0; i < num_of_player; i++){
-        //arr[i].properties += arr[i].money;
+        arr[i].properties += arr[i].money;
         if(arr[i].land_list.size()!=0){
             for(int j = 0; j < arr[i].land_list.size(); j++){
-                arr[i].properties += board[arr[i].land_list[j]].price + 50 * board[arr[i].land_list[j]].property + arr[i].money;
+                arr[i].properties += board[arr[i].land_list[j]].price + 50 * board[arr[i].land_list[j]].property;
             }
             cout<< arr[i].properties<<endl;
-        }
-        else{
-            arr[i].properties = arr[i].money;
         }
 
         if(arr[i].properties >= max_amount){
