ca6cae910418086c81d9cfeb5e61fa17df363fe0
swh2004 <146440509+swh2004@users.noreply.github.com>
2023-11-23 20:12:34 +0800

    Update wincheck.cpp

--- wincheck.cpp
+++ wincheck.cpp
@@ -8,31 +8,31 @@ using namespace std;
 
 //A function to check which player in arr is a win after specific rounds
 //print the name(s) of the winner(s)
-
-
 void outloop_wincheck(Player *arr, int num_of_player, Cell board[]){
-    //const int money_to_win=1000;
     int max_amount = 0;
     vector<string> winner;
-    
+    //calculate the properties of the player
     for(int i = 0; i < num_of_player; i++){
+        //add the money of player
         arr[i].properties += arr[i].money;
         if(arr[i].land_list.size()!=0){
+            //add all the lands and properties on these lands
             for(int j = 0; j < arr[i].land_list.size(); j++){
                 arr[i].properties += board[arr[i].land_list[j]].price + 50 * board[arr[i].land_list[j]].property;
             }
-            //cout<< arr[i].properties<<endl;
         }
-
+        //get highest money
         if(arr[i].properties >= max_amount){
             max_amount = arr[i].properties;
         } 
     }
+    //push winner(s) to the vector
     for(int i = 0; i < num_of_player; i++){
         if(arr[i].properties == max_amount){
             winner.push_back(arr[i].name);
         }
     }
+    //declare the winner(s)
     if(winner.size()>1){
         cout << ">> The winners are ";
         for(int i = 0; i < winner.size(); i++){
@@ -44,22 +44,12 @@ void outloop_wincheck(Player *arr, int num_of_player, Cell board[]){
         cout << ">> The winner is "<< winner[0]<< endl;
     }
 }
-/*if(p1.money >= money_to_win || p2.money <= 0){
-        cout<<"Player 1 wins!"<<endl;
-        return true;
-    }
-    else if(p2.money >= money_to_win || p1.money <= 0){
-        cout<<"Player 2 wins!"<<endl;
-        return true;
-    }
-    return false;
-}*/
+
 
 //A function to check if player p is broke
 //if so return true to eliminate player p
 bool brokecheck(Player p){
     if(p.money < 0){
-        //cout << ">> Player " << p.name << " is broke." << endl;
         return true;
     }
 return false;
