f5b03e55700785b02fcb9ac5587a09d706d593c5
JoeCFKwok <146162566+JoeCFKwok@users.noreply.github.com>
2023-11-26 12:18:24 +0800

    Update printBoard.cpp

--- printBoard.cpp
+++ printBoard.cpp
@@ -9,6 +9,14 @@ const int CELL_WIDTH = 25;  // Define a constant for cell width
 const std::string LINE = std::string(CELL_WIDTH + 2, '-');
 const std::string SPACE = std::string(CELL_WIDTH, ' ');
 
+std::string repeatString(const std::string& str, int times) {
+    std::ostringstream os;
+    for(int i = 0; i <= times; i++)
+        os << str;
+    return os.str();
+}
+
+
 void printBoard(Cell Board[]) {
     int side = BOARD_SIZE / 4;
 
@@ -41,26 +49,26 @@ void printBoard(Cell Board[]) {
     // Print cells from the left and right columns
     for (int i = 1; i < side; i++) {
         // Left column
-        std::cout << "| " << std::setw(CELL_WIDTH - 1) << std::left << Board[4*side - i].name << "|" << SPACE;
+        std::cout << "| " << std::setw(CELL_WIDTH - 1) << std::left << Board[4*side - i].name << "|" << repeatString(SPACE, 9).substr(0, repeatString(SPACE, 9).size()-7);
         // Right column
         std::cout << "| " << std::setw(CELL_WIDTH - 1) << std::left << Board[side + i].name << "|";
         std::cout << std::endl;
-        std::cout << "|" << SPACE << "|" << SPACE << "|";
+        std::cout << "|" << SPACE << "|" << repeatString(SPACE, 9).substr(0, repeatString(SPACE, 9).size()-7) << "|" << SPACE << "|";
         std::cout << std::endl;
         // Left column
-        if (Board[3*side - i].type == 0) {  // If the cell is a Land
-            std::cout << "| $" << std::setw(CELL_WIDTH - 2) << Board[4*side - i].price << "|" << SPACE;
+        if (Board[4*side - i].type == 0) {  // If the cell is a Land
+            std::cout << "| $" << std::setw(CELL_WIDTH - 2) << Board[4*side - i].price << "|" << repeatString(SPACE, 9).substr(0, repeatString(SPACE, 9).size()-7);
         } else {
-            std::cout << "|" << SPACE << "|" << SPACE;
+            std::cout << "|" << SPACE << "|" << repeatString(SPACE, 9).substr(0, repeatString(SPACE, 9).size()-7);
         }
         // Right column
         if (Board[side + i].type == 0) {  // If the cell is a Land
             std::cout << "| $" << std::setw(CELL_WIDTH - 2) << Board[side + i].price << "|";
         } else {
-            std::cout << "|" << SPACE << "|";
+            std::cout << "|" << SPACE << "|" << SPACE;
         }
         std::cout << std::endl;
-        std::cout << LINE << SPACE << LINE;
+        std::cout << LINE << repeatString(SPACE, 9).substr(0, repeatString(SPACE, 9).size()-7) << LINE;
         std::cout << std::endl;
     }
 
